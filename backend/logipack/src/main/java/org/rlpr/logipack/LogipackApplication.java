fb77e926f6b2914c499666ce5f5f5d21e15df7d8
Pedro Rodrigues <pedrofrodrigues4@gmail.com>
2022-11-27 17:58:55 +0000

    Add Spring-Boot project

---
+++ backend/logipack/src/main/java/org/rlpr/logipack/LogipackApplication.java
@@ -0,0 +1,13 @@
+package org.rlpr.logipack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogipackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogipackApplication.class, args);
+	}
+
+}
