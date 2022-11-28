fcf2548999118d13067c69e8397d8a99a782414b
RafaelGoncalvesUA <rfg@ua.pt>
2022-11-28 22:10:09 +0000

    Update Localizacao, Encomenda

--- backend/src/main/java/org/rlpr/logipack/model/Localizacao.java
+++ backend/src/main/java/org/rlpr/logipack/model/Localizacao.java
@@ -1,5 +1,6 @@
 package org.rlpr.logipack.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +8,12 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "tbl_localizacao")
 public class Localizacao {
+    @Id
+    @GeneratedValue
+    private int id;
     private String distrito;
     private String concelho;
     private String freguesia;
