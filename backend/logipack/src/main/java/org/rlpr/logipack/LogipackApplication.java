8fbcaab29cb32d8bead7515de45062543ff7d32a
rafael <rafael.lixa@ua.pt>
2022-11-28 08:51:50 +0000

    Add SprintBoot Receiver

--- backend/logipack/src/main/java/org/rlpr/logipack/LogipackApplication.java
+++ backend/logipack/src/main/java/org/rlpr/logipack/LogipackApplication.java
@@ -2,9 +2,53 @@ package org.rlpr.logipack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.context.annotation.Bean;
+
+
+import org.rlpr.logipack.receiver.Receiver;
+
 
 @SpringBootApplication
 public class LogipackApplication {
+
+	static final String topicExchangeName = "LogiPack";
+
+	@Bean
+	Queue queue1() {return new Queue("queue1", false);}
+	Queue queue2() {return new Queue("queue2", false);}
+
+	@Bean
+	TopicExchange exchange() {
+		return new TopicExchange(topicExchangeName);
+	}
+
+	@Bean
+	Binding binding(Queue queue, TopicExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange).with("*");
+	}
+
+	@Bean
+	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter) {
+		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.setQueueNames("queue1", "queue2");
+		container.setMessageListener(listenerAdapter);
+		return container;
+	}
+  
+	@Bean
+	MessageListenerAdapter listenerAdapter(Receiver receive) {
+	  return new MessageListenerAdapter(receive, "listen");
+	}
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(LogipackApplication.class, args);
---
+++ backend/logipack/src/main/java/org/rlpr/logipack/receiver/Receiver.java
@@ -0,0 +1,22 @@
+package org.rlpr.logipack.receiver;
+
+import java.util.concurrent.CountDownLatch;
+import org.json.JSONObject;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    public void listen(byte[] data) {
+        String strData = new String(data);
+        JSONObject jsonData = new JSONObject(strData);
+        System.out.printf("Message from: %s\n", jsonData.getString("queue"));
+
+        //dispatch the queue to a specific service
+    }
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+}
