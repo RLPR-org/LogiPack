0409122bc141d2870bb7c01a251834d71bb32687
rafael <rafael.lixa@ua.pt>
2022-11-28 09:57:26 +0000

    Logging Service

--- backend/logipack/src/main/java/org/rlpr/logipack/receiver/Receiver.java
+++ backend/logipack/src/main/java/org/rlpr/logipack/receiver/Receiver.java
@@ -4,16 +4,22 @@ import java.util.concurrent.CountDownLatch;
 import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 
+import org.rlpr.logipack.service.LoggingSevice;
+
 @Component
 public class Receiver {
     private CountDownLatch latch = new CountDownLatch(1);
+
+    private LoggingSevice loggingSevice;
+
 
     public void listen(byte[] data) {
         String strData = new String(data);
         JSONObject jsonData = new JSONObject(strData);
         System.out.printf("Message from: %s\n", jsonData);
 
-        //dispatch the queue to a specific service
+        loggingSevice.CarLog(jsonData);
+
     }
 
     public CountDownLatch getLatch() {
