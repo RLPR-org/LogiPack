2b33a35bda3d9520b7a0375d7d0c2b9ade1f44e4
rafael <rafael.lixa@ua.pt>
2022-11-28 11:25:08 +0000

    Update do Logging Service

--- backend/logipack/src/main/java/org/rlpr/logipack/service/LoggingSevice.java
+++ backend/logipack/src/main/java/org/rlpr/logipack/service/LoggingSevice.java
@@ -4,13 +4,28 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.stereotype.Service;
 
+import org.rlpr.logipack.service.AppService;
 import org.rlpr.logipack.model.*;
 import org.rlpr.logipack.repository.*;
 
 public class LoggingSevice {
 
+    @Autowired
+    private AppService appService;
+
     public void CarLog(JSONObject data) {
         System.out.println("appends the car Log -> " +  data);
+
     }
-    
+
+    public void reciptConfimation(JSONObject data){
+
+        int id = data.getInt("encomenda_id");
+
+        Estado estado = Estado.ENTREGUE;
+        
+        appService.updateEstado(estado,id);
+
+    }
+
 }
