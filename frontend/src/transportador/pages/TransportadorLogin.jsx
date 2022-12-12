bf34892f46e479005deb67c1a6ff279afbe41ee5
Leonardo Almeida <leonardoalmeida7@ua.pt>
2022-12-12 09:57:40 +0000

    Add Login forms and update routes

---
+++ frontend/src/transportador/pages/TransportadorLogin.jsx
@@ -0,0 +1,44 @@
+import '../../App.css';
+
+import Container from '@mui/material/Container';
+import { Button } from '@mui/material/';
+import { TextField } from '@mui/material/';
+import { Grid } from '@mui/material/';
+
+import { useNavigate } from 'react-router-dom';
+
+function TransportadorLogin() {
+
+    const navigate = useNavigate();
+
+    const handleLogin = () => {
+        console.log('email: ', document.getElementById('email').value);
+        console.log('password: ', document.getElementById('password').value);
+        navigate('/transportador/0');
+    }
+
+    return (
+        <>
+            <Container maxWidth="sm">
+                <Grid container justifyContent="center" alignItems="center" style={{height: '100vh'}}>
+                    <Grid item style={{
+                        textAlign: 'center', 
+                        border: '1px solid #000', 
+                        padding: '20px', 
+                        borderRadius: '10px',
+                        boxShadow: '0 0 10px lightgray',
+                        borderColor: 'lightgray',
+                        width: '100%'
+                        }}>
+                        <h1>Login</h1>
+                        <TextField id="email" label="Email" variant="outlined" style={{marginBottom: '30px', width: '100%'}} />
+                        <TextField id="password" label="Senha" variant="outlined" style={{marginBottom: '30px', width: '100%'}} />
+                        <Button variant="contained" color="primary" style={{width: '200px', height: '50px'}} onClick={handleLogin}>Entrar</Button>
+                    </Grid>
+                </Grid>
+            </Container>
+        </>
+    )
+}
+
+export default TransportadorLogin;
