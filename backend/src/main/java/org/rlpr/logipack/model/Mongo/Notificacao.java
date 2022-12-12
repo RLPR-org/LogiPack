7573a2ae7204cc1f3d658421a238ec919d603eba
Pedro Rodrigues <pedrofrodrigues4@gmail.com>
2022-12-12 10:03:08 +0000

    Add client endpoints for notifications

---
+++ backend/src/main/java/org/rlpr/logipack/model/Mongo/Notificacao.java
@@ -0,0 +1,17 @@
+package org.rlpr.logipack.model.Mongo;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document("cliente_notificacoes")
+public class Notificacao {
+    private int encomendaId;
+    private String message;
+    private String timestamp; 
+}
+
