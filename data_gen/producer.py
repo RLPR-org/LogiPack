868ac27cc599c2a30b797663203ee2debed6188d
RafaelGoncalvesUA <rfg@ua.pt>
2022-12-12 23:06:42 +0000

    Merge RabbitMQ queues

--- data_gen/producer.py
+++ data_gen/producer.py
@@ -3,7 +3,7 @@ import json
 import time
 
 with open("dataset.json", "r", encoding='utf-8') as f:
-    registos = json.load(f)
+    registos_src = json.load(f)
 
 while True:
     try:
@@ -18,29 +18,24 @@ channel = connection.channel()
 
 channel.exchange_declare("LogiPack", durable=True, exchange_type="topic")
 
-channel.queue_declare(queue= "queue_encomendas")
-channel.queue_bind(exchange="LogiPack", queue="queue_encomendas", routing_key="queue_encomendas")
+channel.queue_declare(queue= "queue_logipack")
+channel.queue_bind(exchange="LogiPack", queue="queue_logipack", routing_key="queue_logipack")
 
-channel.queue_declare(queue= "queue_transportadores")
-channel.queue_bind(exchange="LogiPack", queue="queue_transportadores", routing_key="queue_transportadores")
-
-channel.queue_declare(queue= "queue_clientes")
-channel.queue_bind(exchange="LogiPack", queue="queue_clientes", routing_key="queue_clientes")
-
+registos = []
 while True:
+    if not registos:
+        registos = registos_src.copy()
     registo = registos.pop(0)
     prox_registo = registos[0]
-    if "password_hash" in registo:
-        channel.basic_publish(exchange='LogiPack', routing_key="queue_clientes", body=json.dumps(registo))
+    channel.basic_publish(exchange='LogiPack', routing_key="queue_logipack", body=json.dumps(registo))
+    if registo["entity"] == "cliente":
         print(" [x] (Cliente) Sent %r" % registo, flush=True)
-    elif "encomenda" in registo:
-        channel.basic_publish(exchange='LogiPack', routing_key="queue_encomendas", body=json.dumps(registo))
+    elif registo["entity"] == "encomenda":
         print(" [x] (Encomenda) Sent %r" % registo, flush=True)
-        if "encomenda" not in prox_registo: # é um novo transportador
+        if prox_registo["entity"] == "transportador":
             time.sleep(10)
             continue
         time.sleep(1)
     else:
-        channel.basic_publish(exchange='LogiPack', routing_key="queue_transportadores", body=json.dumps(registo))
         print(" [x] (Transportador) Sent %r" % registo, flush=True)
         time.sleep(1)
