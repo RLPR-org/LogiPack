874f77a6fd152ee0bcc6ae006e430bdf7ef2e8c6
Pedro Rodrigues <pedrofrodrigues4@gmail.com>
2022-12-13 10:46:25 +0000

    LOG-16 Update backend to allow delete notifications

--- backend/src/main/java/org/rlpr/logipack/LogipackApplication.java
+++ backend/src/main/java/org/rlpr/logipack/LogipackApplication.java
@@ -21,7 +21,7 @@ public class LogipackApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
 			}
 		};
 	}
