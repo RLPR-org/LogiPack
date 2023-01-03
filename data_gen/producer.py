6113b0df75cc8bbb6015fdab422de77f22e945e3
rfg@ua.pt <rfg@ua.pt>
2023-01-03 11:14:55 +0000

    Update sleep - datagen

--- data_gen/producer.py
+++ data_gen/producer.py
@@ -36,9 +36,9 @@ while True:
     elif registo["entity"] == "encomenda":
         print(" [x] (Encomenda) Sent %r" % registo, flush=True)
         if prox_registo["entity"] == "transportador":
-            time.sleep(random.randint(5, 20) * 60)
+            time.sleep(random.randint(1, 3) * 60)
             continue
-        time.sleep(random.randint(5, 20) * 60)
+        time.sleep(random.randint(1, 3) * 60)
     else:
         print(" [x] (Transportador) Sent %r" % registo, flush=True)
-        time.sleep(random.randint(5, 20) * 60)
+        time.sleep(random.randint(1, 3) * 60)
